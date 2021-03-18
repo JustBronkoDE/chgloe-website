43ca74661419465b6f099df6711a19d140ee4c9f
Lamine Dia <lamine.dia@visuellverstehen.de>
2021-03-18 13:40:18 +0100

    Combine Home and HomeMenu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,34 @@
 <template>
     <main class="home">
-        <HomeMenu />
+        <ul class="home__menu">
+            <li class="home__menu-item" @click="openView('about-me')">
+                <span class="title title--bold title--bright">About me</span>
+                <video class="home__menu-image" loop muted preload="metadata" @mouseover="$event.target.play()" @mouseleave="$event.target.pause()">
+                    <source src="@/assets/images/about-me.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </li>
+            <li class="home__menu-item" @click="openView('gallery')">
+                <span class="title title--bold title--bright">My Gallery</span>
+                <video class="home__menu-image" loop muted preload="metadata" @mouseover="$event.target.play()" @mouseleave="$event.target.pause()">
+                    <source src="@/assets/images/gallery.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </li>
+        </ul>
     </main>
 </template>
 
 <script lang="ts">
-    import HomeMenu from '@/components/HomeMenu.vue';
+    import { defineComponent } from 'vue';
 
-    export default {
-        components: {
-            HomeMenu,
-        },
-    }
+    export default defineComponent({
+        methods: {
+            openView(view: string) {
+                this.$router.push(view)
+            }
+        }
+    })
 </script>
 
 <style lang="scss" scoped>
@@ -22,5 +39,54 @@
         display: flex;
         justify-content: center;
         align-items: center;
+    }
+
+        .home__menu {
+        display: flex;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+        animation: fade-in 1s $transition-curve;
+    }
+
+    .home__menu-item {
+        height: 95%;
+        margin-right: $spacing-standard;
+        cursor: pointer;
+        transition: height $transition-speed-fast $transition-curve;
+        text-align: center;
+        text-decoration: none;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        flex: 1 1 0px;
+
+        &:last-child {
+            margin-right: 0;
+        }
+
+        &:hover {
+            height: 100%;
+            
+            .home__menu-image {
+                filter: brightness(0.9);
+            }
+        }
+
+        .title {
+            position: absolute;
+            z-index: $z-index-s;
+        }
+    }
+
+    .home__menu-image {
+        box-shadow: $box-shadow-light;
+        object-fit: cover;
+        object-position: top;
+        height: 85%;
+        width: 100%;
+        filter: brightness(0.6);
+        transition: all $transition-speed-fast $transition-curve;
     }
 </style>
